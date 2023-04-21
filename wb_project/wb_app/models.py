73e8430a38886782432948c0e1b5f4e9aa6847aa
AKA <ksunyaef@gmail.com>
2023-04-22 00:36:10 +0300

    models

--- wb_project/wb_app/models.py
+++ wb_project/wb_app/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+    quantity =models.IntegerField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+
+class Transactions(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    client = models.ForeignKey(User, on_delete=models.CASCADE)
